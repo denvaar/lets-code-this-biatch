ad1ed124e01c1e5f6016ecf89e08143c9a7b0410
Denver Smith <denverpsmith@gmail.com>
2016-11-19 13:42:16 -0700

    Revert "part 1"

--- components/todoItem.jsx
+++ components/todoItem.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 const TodoItem = (props) => {
   return (
     <div className="todo-item">
+      <p className={props.completed ? "strike" : ""} 
+         onClick={()=>{return props.markComplete(props.index)}}>
+        {props.text}
+      </p>
     </div>
   );  
 }
